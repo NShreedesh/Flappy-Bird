cd0d25007e4fa8b15b3761bbd665a3b78bf9c649
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-07-31 09:11:22 +0545

    add fly animation and gamemanager

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Player
@@ -8,6 +7,8 @@ namespace Player
         [Header("Bird Components")]
         [SerializeField]
         private new Rigidbody2D rigidbody;
+        [SerializeField]
+        private Animator animator;
 
         [Header("Other Components")]
         [SerializeField]
@@ -19,12 +20,25 @@ namespace Player
         [SerializeField]
         private float jumpForce = 5f;
 
+        [Header("Animator Tags")]
+        private const string CanFly = "canFly";
+
+        [Header("Animator Tags Hash")]
+        private int _canFly;
+
+        private void Start()
+        {
+            _canFly = Animator.StringToHash(CanFly);
+        }
+
         private void Update()
         {
             if (inputController.PressInputAction.triggered)
             {
                 canJump = true;
             }
+            
+            PlayAnimation();
         }
         
         private void FixedUpdate()
@@ -39,6 +53,21 @@ namespace Player
             rigidbody.velocity = Vector2.zero;
             rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             canJump = false;
+        }
+
+        private void PlayAnimation()
+        {
+            var canFly = animator.GetBool(_canFly);
+
+            switch (rigidbody.velocity.y)
+            {
+                case >= -4f when !canFly:
+                    animator.SetBool(_canFly, true);
+                    break;
+                case < -4f when canFly:
+                    animator.SetBool(_canFly, false);
+                    break;
+            }
         }
     }
 }
