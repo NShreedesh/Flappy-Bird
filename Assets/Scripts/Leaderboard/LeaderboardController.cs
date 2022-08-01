79add07a75a65eda3eddb8c22a4ff4497c32eac5
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-08-01 18:45:47 +0545

    add leaderboard controller

--- Assets/Scripts/Leaderboard/LeaderboardController.cs
+++ Assets/Scripts/Leaderboard/LeaderboardController.cs
@@ -1,9 +1,53 @@
+using LootLocker.Requests;
 using UnityEngine;
 
 namespace Leaderboard
 {
     public class LeaderboardController : MonoBehaviour
     {
+        private const int LeaderboardId = 5120;
+        private string _memberId;
+        
+        private void Start()
+        { 
+            LootLockerSDKManager.StartGuestSession(SystemInfo.deviceUniqueIdentifier,(response) =>
+            {
+                if (!response.success) return;
+                _memberId = response.player_id.ToString();
+            });
+        }
 
+        private void SubmitScore(int score)
+        {
+            LootLockerSDKManager.SubmitScore(_memberId, score, LeaderboardId, response =>
+            {
+                if (response.success)
+                {
+                    
+                }
+                else
+                {
+                    
+                }
+            });
+        }
+
+        private void GetLeaderboard()
+        {
+            LootLockerSDKManager.GetScoreList(LeaderboardId, 10, 0, response =>
+            {
+                if (response.success)
+                {
+                    foreach (var item in response.items)
+                    {
+                        print(item.member_id + " " + item.score);
+                    }
+                }
+                else
+                {
+                    
+                }
+            });
+        }
     }
 }
